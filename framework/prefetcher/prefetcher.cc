e522f78d4f654cf396e14a36d5d35232840a4ef1
andernil <annil@stud.ntnu.no>
2018-02-23 13:33:35 +0100

    Working RPT prefetcher, 1.06

--- framework/prefetcher/prefetcher.cc
+++ framework/prefetcher/prefetcher.cc
@@ -5,30 +5,97 @@
  */
 
 #include "interface.hh"
+#include <map>
 
-
+//Struct for the Reference Prediction Table
+/*State:
+1=initialize
+2=transient
+3=steady
+4=no-prediction*/
+struct RPTable{
+  Addr addrPc, addrPrevMem;
+  int stride, state;
+};
+int diff = 0;
+//Map with the address of the instruction as key and a RPT as element
+static std::map<Addr,RPTable> mapRPT;
 void prefetch_init(void)
 {
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
 
-    //DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
+    DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
 }
+
+void prefetch_now(AccessStat stat){
+  //Checking if the new instruction adress is in the table, if not is is added.
+  //Case A.1.
+  if(mapRPT.end() == mapRPT.find(stat.pc)){
+    mapRPT[stat.pc].addrPc = stat.pc;
+    mapRPT[stat.pc].addrPrevMem = stat.mem_addr;
+    mapRPT[stat.pc].stride = 0;
+    mapRPT[stat.pc].state = 1;
+  }
+  //If it already is in the table, the entry is updated.
+  //Case A.2.
+  
+  else{
+    diff = stat.mem_addr - mapRPT[stat.pc].addrPrevMem;
+    switch(mapRPT[stat.pc].state){
+    case 1: //Initial
+      if (mapRPT[stat.pc].stride == diff)
+        mapRPT[stat.pc].state = 3;
+      else {
+        mapRPT[stat.pc].stride = diff;
+        mapRPT[stat.pc].state = 2;
+      }
+      mapRPT[stat.pc].addrPrevMem = stat.mem_addr;
+      break;
+    case 2: //Transient
+      if (mapRPT[stat.pc].stride == diff)
+        mapRPT[stat.pc].state = 3;
+      else{
+        mapRPT[stat.pc].stride = diff;
+        mapRPT[stat.pc].state = 4;
+      }
+      mapRPT[stat.pc].addrPrevMem = stat.mem_addr;
+      break;
+    case 3: //Steady
+      if (mapRPT[stat.pc].stride != diff){
+        mapRPT[stat.pc].state = 1;
+      }
+      else{
+        mapRPT[stat.pc].state = 3;
+      }
+      mapRPT[stat.pc].addrPrevMem = stat.mem_addr;
+      break;
+    case 4: //No prediction
+      if (mapRPT[stat.pc].stride == diff)
+        mapRPT[stat.pc].state = 2;
+      else{
+        mapRPT[stat.pc].stride = diff;
+        mapRPT[stat.pc].state = 4;
+      }
+      mapRPT[stat.pc].addrPrevMem = stat.mem_addr;
+      break;
+    default:
+      mapRPT[stat.pc].state = 1;
+      break;
+    }
+  }
+  if(!in_cache(stat.mem_addr+(uint64_t)mapRPT[stat.pc].stride) && mapRPT[stat.pc].state == 3){
+    issue_prefetch(stat.mem_addr+(uint64_t)mapRPT[stat.pc].stride);
+    DPRINTF(HWPrefetch, "Prefetching\n");
+  }
+}
+
+
 
 void prefetch_access(AccessStat stat)
 {
-    /* pf_addr is now an address within the _next_ cache block */
-    Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
-
-    /*
-     * Issue a prefetch request if a demand miss occured,
-     * and the block is not already in cache.
-     */
-    if (stat.miss && !in_cache(pf_addr)) {
-        issue_prefetch(pf_addr);
-    }
+  prefetch_now(stat);
 }
-
 void prefetch_complete(Addr addr) {
     /*
      * Called when a block requested by the prefetcher has been loaded.
